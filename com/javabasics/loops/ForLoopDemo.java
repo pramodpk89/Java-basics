9bc869bdfaf652e12f6bafc949b52c1e6384d89d
pramodp <pramodpk89@gmail.com>
2023-09-28 07:49:10 +0530

    for loop demo

--- com/javabasics/loops/ForLoopDemo.java
+++ com/javabasics/loops/ForLoopDemo.java
@@ -1,7 +1,21 @@
 package com.javabasics.loops;
 
+import java.util.Arrays;
+
 public class ForLoopDemo {
     public static void main(String[] args) {
+        String[] cities=new String[]{"Delhi","Bangalore","New york","Ottawa"};
+
+        for (int i=0;i<=1;i++){
+            System.out.println("City name is "+cities[i]+" index is "+i);
+        }
+
+
+        int[] array= {100,200,300,400,500};
+        for(int value: array){
+            System.out.println(value);
+        }
+
 
     }
 }
