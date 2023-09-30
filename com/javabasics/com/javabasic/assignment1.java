d680d14705eb291a5759e7316fd4ba9a9d18e1f4
pramodp <pramodpk89@gmail.com>
2023-09-30 08:12:08 +0530

    number divisible by 3 and 5 but not both

---
+++ com/javabasics/com/javabasic/assignment1.java
@@ -0,0 +1,24 @@
+package com.javabasics.com.javabasic;
+
+import java.util.Scanner;
+
+public class assignment1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = scanner.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 != divisibleBy5) { // this condition will be true if the number is divisible by either 3 or 5 but not both.
+                sum += i;
+            }
+        }
+
+        System.out.println("The sum is: " + sum);
+    }
+}
